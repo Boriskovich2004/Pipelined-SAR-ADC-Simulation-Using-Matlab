ad75393db195e191f346055d995b2af3a7c68a5c
Boris Jia <3141000651@qq.com>
2025-06-14 19:33:26 +0800

    Update Report & RA_FUN, adjust noise and offset

--- Code/RA_FUN.m
+++ Code/RA_FUN.m
@@ -3,7 +3,8 @@ function [Voutp, Voutn] = RA_FUN(Vinp, Vinn, Av, GBW, Cs, Ch, Vcm, offset, noise
 Av_linear = 10^(Av/20); % dB to linear gain
 beta = Cs/Ch;
 Av_close = Av_linear/(1 + Av_linear*beta);
-Vin_diff = Vinp - Vinn;
+% 添加失调与热噪声
+Vin_diff = Vinp - Vinn + offset + noise * randn(size(Vinp));
 Vres_diff = Vin_diff * Av_close;
 
 % 模拟放大器带宽限制影响（可选滤波建模）
@@ -11,8 +12,7 @@ tau = 1 / (2 * pi * GBW); % 时间常数
 alpha = exp(-1 / (fs * tau));
 Vres_filtered = filter(1 - alpha, [1, -alpha], Vres_diff);
 
-% 添加失调与热噪声
-Vres_final = Vres_filtered + offset + noise * randn(size(Vinp));
+Vres_final = Vres_filtered;
 
 % 转为差模输出
 Voutp = Vcm + Vres_final / 2;
